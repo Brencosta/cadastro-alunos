155c261b71330345dca6e2eb4b085ddbfd98cebc
Breno cunha <brenocunha977@gmail.com>
2026-06-15 09:05:55 -0300

    feat: feito crud de cadastrar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from models import aluno
 app = Flask (__name__)
@@ -12,7 +12,12 @@ with app.app_context():  # ta criando a tabela no banco de dados, tem que ser de
 
 @app.route("/cadastrar", methods =["POST"])
 def cadastrar():
-    
+    nome = request.form.get("nome")
+    idade = request.form.get("idade")
+    aluno_novo = aluno(nome=nome, idade=idade)
+    db.session.add(aluno_novo)
+    db.session.commit()
+    return "Aluno cadastrado com sucesso!"
 
 
 
